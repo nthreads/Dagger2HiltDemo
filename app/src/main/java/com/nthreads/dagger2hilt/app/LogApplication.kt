0513dbc2fbd0a358080af9cefca4c9fec5532566
Nauman Zubair <nauman.zubair@gmail.com>
2021-11-21 16:36:07 +0400

    Removed ServiceLocator after DI to provide Database

--- app/src/main/java/com/nthreads/dagger2hilt/app/LogApplication.kt
+++ app/src/main/java/com/nthreads/dagger2hilt/app/LogApplication.kt
@@ -7,10 +7,4 @@ import dagger.hilt.android.HiltAndroidApp
 @HiltAndroidApp
 open class LogApplication : Application() {
 
-    lateinit var serviceLocator: ServiceLocator
-
-    override fun onCreate() {
-        super.onCreate()
-        serviceLocator = ServiceLocator(applicationContext)
-    }
 }
