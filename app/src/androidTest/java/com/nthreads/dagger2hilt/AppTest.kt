5cb1543b9887d07f4db4892078d85d2af8a912b7
Nauman Zubair <nauman.zubair@gmail.com>
2021-11-25 12:52:43 +0400

    Added tests

---
+++ app/src/androidTest/java/com/nthreads/dagger2hilt/AppTest.kt
@@ -0,0 +1,15 @@
+package com.nthreads.dagger2hilt
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
+import org.junit.Rule
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+@HiltAndroidTest
+class AppTest {
+
+    @get:Rule
+    var hiltRule = HiltAndroidRule(this)
+}
--- app/src/main/java/com/nthreads/dagger2hilt/data/LogDao.kt
+++ app/src/main/java/com/nthreads/dagger2hilt/data/LogDao.kt
@@ -16,6 +16,7 @@
 
 package com.nthreads.dagger2hilt.data
 
+import android.database.Cursor
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.Query
@@ -35,4 +36,10 @@ interface LogDao {
 
     @Query("DELETE FROM logs")
     fun nukeTable()
+
+    @Query("SELECT * FROM logs ORDER BY id DESC")
+    fun selectAllLogsCursor(): Cursor
+
+    @Query("SELECT * FROM logs WHERE id = :id")
+    fun selectLogById(id: Long): Cursor?
 }
